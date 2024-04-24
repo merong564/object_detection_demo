cab73f3b3f9999b69ed3100f3f30cc70332b553b
merong564 <161675231+merong564@users.noreply.github.com>
2024-04-24 15:49:11 +0900

    test_for_exam

--- export_inference_graph.py
+++ export_inference_graph.py
@@ -10,7 +10,7 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-# limitations under the License.56
+# limitations under the License.
 # ==============================================================================
 
 r"""Tool to export an object detection model for inference.
